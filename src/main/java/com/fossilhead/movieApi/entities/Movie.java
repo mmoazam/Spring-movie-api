9b05a883e9883daa2d27a85e082f223aaf45a5ea
mmoazam <mmoazam@users.noreply.github.com>
2024-08-16 20:00:58 +0100

    Add Movie entity

--- src/main/java/com/fossilhead/movieApi/entities/Movie.java
+++ src/main/java/com/fossilhead/movieApi/entities/Movie.java
@@ -1,4 +1,46 @@
 package com.fossilhead.movieApi.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long movieId;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Title cannot be blank.")
+    private String title;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Director cannot be blank.")
+    private String director;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Studio cannot be blank.")
+    private String studio;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_cast")
+    private Set<String> movieCast;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Release year cannot be blank.")
+    private Integer releaseYear;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Poster cannot be blank.")
+    private String poster;
+
+
 }
