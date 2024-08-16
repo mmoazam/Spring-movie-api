9bcaeca45af6c7e4c35ca6336ec69ae219548ed4
mmoazam <mmoazam@users.noreply.github.com>
2024-08-16 21:23:33 +0100

    add MovieDto

--- src/main/java/com/fossilhead/movieApi/dto/MovieDto.java
+++ src/main/java/com/fossilhead/movieApi/dto/MovieDto.java
@@ -1,45 +1,30 @@
-package com.fossilhead.movieApi.entities;
+package com.fossilhead.movieApi.dto;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class Movie {
+public class MovieDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long movieId;
 
-    @Column(nullable = false)
     @NotBlank(message = "Title cannot be blank.")
     private String title;
 
-    @Column(nullable = false)
     @NotBlank(message = "Director cannot be blank.")
     private String director;
 
-    @Column(nullable = false)
     @NotBlank(message = "Studio cannot be blank.")
     private String studio;
 
-    @ElementCollection
-    @CollectionTable(name = "movie_cast")
     private Set<String> movieCast;
 
-    @Column(nullable = false)
     private Integer releaseYear;
 
-    @Column(nullable = false)
     @NotBlank(message = "Poster cannot be blank.")
     private String poster;
 
-
+    @NotBlank(message = "PosterUrl cannot be blank.")
+    private String posterUrl;
 }
