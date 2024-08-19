8d5a4f3b95275610c36e1f48847f7768342a9c34
mmoazam <mmoazam@users.noreply.github.com>
2024-08-19 09:48:32 +0100

    Implement MovieController::getMovieById MovieController::getAllMovies

--- src/main/java/com/fossilhead/movieApi/controllers/MovieController.java
+++ src/main/java/com/fossilhead/movieApi/controllers/MovieController.java
@@ -8,13 +8,11 @@ import com.fossilhead.movieApi.service.MovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -31,6 +29,17 @@ public class MovieController {
                                              @RequestPart String movieDto) throws IOException {
         MovieDto dto =  convertToJsonMovieDto(movieDto);
         return new ResponseEntity<>(movieService.addMovie(dto, file), HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{movieId}")
+    public ResponseEntity<MovieDto> getMovieById(@PathVariable Long movieId){
+       return new ResponseEntity<>(movieService.getMovie(movieId), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieDto>> getAllMovies(){
+        return  ResponseEntity.ok(movieService.getAllMovies());
     }
 
     private MovieDto convertToJsonMovieDto(String movieDtoObj) throws JsonProcessingException {
